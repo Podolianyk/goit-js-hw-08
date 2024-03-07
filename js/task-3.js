72ce03b4e30a8587040e1bb418a1fef899bd9099
Podolianyk <podolianik1988@gmail.com>
2024-03-07 15:08:12 +0100

    homework

--- js/task-3.js
+++
@@ -1,9 +0,0 @@
-const inputEl = document.getElementById("name-input");
-const outputEl = document.getElementById("name-output");
-
-inputEl.addEventListener("input", handleInput);
-
-function handleInput() {
-  let name = inputEl.value.trim();
-  outputEl.textContent = name || "Anonymous";
-}
